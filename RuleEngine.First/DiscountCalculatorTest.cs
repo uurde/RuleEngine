775f96077b8787d43108927daad45fddf56b2f15
Uğur Değirmenci <m.ugurdegirmenci@gmail.com>
2025-03-11 12:06:53 +0300

    more tests

--- RuleEngine.First/DiscountCalculatorTest.cs
+++ RuleEngine.First/DiscountCalculatorTest.cs
@@ -34,6 +34,74 @@ public class DiscountCalculatorTest
         result.Should().Be(.15m);
     }
 
+    [Theory]
+    [InlineData(20)]
+    [InlineData(70)]
+    public void Returns10ForCustomerVeteran(int customerAge)
+    {
+        var customer = CreateCustomer(customerAge, DateTime.Today.AddDays(-1));
+        customer.IsVeteran = true;
+        var result = _discountCalculator.CalculateDiscountPercentage(customer);
+        result.Should().Be(.10m);
+    }
+
+    [Theory]
+    [InlineData(1, .05)]
+    [InlineData(2, .08)]
+    [InlineData(5, .10)]
+    [InlineData(10, .12)]
+    [InlineData(15, .15)]
+    public void ReturnsCorrentLoyaltyDiscout(int yearsOfCustomer, decimal expectedDiscount)
+    {
+        var customer = CreateCustomer(DefaultAge, DateTime.Today.AddYears(-yearsOfCustomer).AddDays(-1));
+        var result = _discountCalculator.CalculateDiscountPercentage(customer);
+        result.Should().Be(expectedDiscount);
+    }
+
+
+    [Theory]
+    [InlineData(1, .15)]
+    [InlineData(2, .18)]
+    [InlineData(5, .20)]
+    [InlineData(10, .22)]
+    [InlineData(15, .25)]
+    public void ReturnsCorrentLoyaltyDiscoutWithBirthday(int yearsOfCustomer, decimal expectedDiscount)
+    {
+        var customer = CreateBirthdayCustomer(DefaultAge, DateTime.Today.AddYears(-yearsOfCustomer).AddDays(-1));
+        var result = _discountCalculator.CalculateDiscountPercentage(customer);
+        result.Should().Be(expectedDiscount);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    public void ReturnsVeteransDiscout1And2YearLoyalty(int yearsOfCustomer)
+    {
+        var customer = CreateCustomer(DefaultAge, DateTime.Today.AddYears(-yearsOfCustomer).AddDays(-1));
+        customer.IsVeteran = true;
+        var result = _discountCalculator.CalculateDiscountPercentage(customer);
+        result.Should().Be(.10m);
+    }
+    
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    public void ReturnsVeteransDiscout1And2YearLoyaltyOnBirthday(int yearsOfCustomer)
+    {
+        var customer = CreateBirthdayCustomer(DefaultAge, DateTime.Today.AddYears(-yearsOfCustomer).AddDays(-1));
+        customer.IsVeteran = true;
+        var result = _discountCalculator.CalculateDiscountPercentage(customer);
+        result.Should().Be(.20m);
+    }
+
+    [Fact]
+    public void Returns10ForCustomerSecondPurchaseOnBirthday()
+    {
+        var customer = CreateBirthdayCustomer(20, DateTime.Today.AddDays(-1));
+        var result = _discountCalculator.CalculateDiscountPercentage(customer);
+        result.Should().Be(.10m);
+    }
+    
     private Customer CreateCustomer(int age = DefaultAge, DateTime? firstPurchaseDate = null)
     {
         return new Customer
